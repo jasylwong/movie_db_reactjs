3201d7e5a73644e5daa57d810c18ed227ee6ca21
Jason <jasylwong@gmail.com>
2020-04-14 16:09:29 +0100

    add build directory

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState } from 'react'
+import axios from 'axios'
 
 import Search from './components/Search'
+import Results from './components/Results'
+import Popup from './components/Popup'
 
 function App() {
   const [state, setState] = useState({
@@ -9,30 +11,44 @@ function App() {
     results: [],
     selected: {}
   });
-
   const apiurl = "http://www.omdbapi.com/?apikey=dfe6d885";
 
   const search = (e) => {
     if (e.key === "Enter") {
       axios(apiurl + "&s=" + state.s).then(({ data }) => {
         let results = data.Search;
-        
+
         setState(prevState => {
           return { ...prevState, results: results }
         })
-        console.log(data.Search[0]);
       });
     }
   }
-
+  
   const handleInput = (e) => {
     let s = e.target.value;
 
     setState(prevState => {
-      return { ...prevState, s: s}
+      return { ...prevState, s: s }
     });
+  }
 
-    // console.log(state.s);
+  const openPopup = id => {
+    axios(apiurl + "&i=" + id).then(({ data }) => {
+      let result = data;
+
+      console.log(result);
+
+      setState(prevState => {
+        return { ...prevState, selected: result }
+      });
+    });
+  }
+
+  const closePopup = () => {
+    setState(prevState => {
+      return { ...prevState, selected: {} }
+    });
   }
 
   return (
@@ -42,9 +58,13 @@ function App() {
       </header>
       <main>
         <Search handleInput={handleInput} search={search} />
+
+        <Results results={state.results} openPopup={openPopup} />
+
+        {(typeof state.selected.Title != "undefined") ? <Popup selected={state.selected} closePopup={closePopup} /> : false}
       </main>
     </div>
   );
 }
 
-export default App;
+export default App
